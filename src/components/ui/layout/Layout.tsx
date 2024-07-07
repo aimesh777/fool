5d227da04fa0439f71b33e87cb250956d3f9f02a
Danil Morozov <dan.project1101@gmail.com>
2024-07-07 16:11:51 +0700

    fix layout

--- src/components/ui/layout/Layout.tsx
+++ src/components/ui/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import cn from 'clsx'
 import { FC, PropsWithChildren } from 'react'
+import { useLocation } from 'react-router-dom'
 
 import { Typography } from '@/components/ui'
 import Logo from '@/components/ui/Logo'
@@ -15,8 +16,14 @@ const Layout: FC<PropsWithChildren<ILayout>> = ({
 	header,
 	footer
 }) => {
+	const route = useLocation()
 	return (
-		<div className='flex flex-col justify-between items-center py-base-x4 mb-base-x4 gap-base-x2 overflow-auto px-[10%] min-h-full'>
+		<div
+			className={cn(
+				'flex flex-col justify-between items-center py-base-x4 mb-base-x4 gap-base-x2 px-[10%] h-full',
+				route.pathname !== '/game' && 'overflow-auto'
+			)}
+		>
 			<div className='flex flex-col items-center gap-base-x4 w-full h-full'>
 				{!noLogo && <Logo />}
 				{header && (
