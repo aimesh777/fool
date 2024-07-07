d3820ee71449731c00de31e81b476455a8b42f25
Danil Morozov <dan.project1101@gmail.com>
2024-07-07 15:37:39 +0700

    fix loading

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
 	}
 
 	useEffect(() => {
-		if (document.readyState === 'complete') {
+		if (document.readyState !== 'loading') {
 			handleLoading()
 		} else {
 			window.addEventListener('DOMContentLoaded', handleLoading)
